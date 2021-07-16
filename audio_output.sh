c52efeca85023c0b3ead185ef40964f2db315cdd
roblescristian4722 <roblescristian4722@gmail.com>
2021-07-15 23:09:07 -0500

    fzf replaced by rofi

--- audio_output.sh
+++ audio_output.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pactl list sinks | awk -F"Sink #" 'NF > 1 { print $2 }' | awk -v out="$1" 'NR == out { cmd = "pactl set-default-sink " $1; system(cmd); }'
+pactl list sinks | awk -F"Destino #" 'NF > 1 { print $2 }' | awk -v out="$1" 'NR == out { cmd = "pactl set-default-sink " $1; system(cmd); }'
